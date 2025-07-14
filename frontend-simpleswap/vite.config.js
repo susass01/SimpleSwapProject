d0e0454757aad262e403e630a47f848ffe5d55eb
Susana <susasssanchez@gmail.com>
2025-07-13 21:36:04 -0300

    Fix vite config for deploy

--- frontend-simpleswap/vite.config.js
+++ frontend-simpleswap/vite.config.js
@@ -1,6 +1,5 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
@@ -8,4 +7,4 @@ export default defineConfig({
   server: {
     port: 5173
   }
-})
+});
